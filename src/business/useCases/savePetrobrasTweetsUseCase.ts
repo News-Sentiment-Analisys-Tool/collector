5999d0ac3f54681cea73454a43bcfbc231e2dddf
Eugenio Sales <eugenio.sales@outlook.com>
2022-03-02 19:44:43 -0300

    Add message service to Bradesco, Itau and Petrobras use case

--- src/business/useCases/savePetrobrasTweetsUseCase.ts
+++ src/business/useCases/savePetrobrasTweetsUseCase.ts
@@ -1,6 +1,7 @@
 import async from 'async'
 import { ITweetRepository } from '../repositories/ITweetRepository'
 import { ITwitterService } from '../services/ITwitterService'
+import { IMessageService } from '../services/IMessageService'
 import { Tweet } from '../../domain/entities/tweet'
 import { COMPANY } from '../../domain/constants/company'
 
@@ -9,8 +10,10 @@ export class SavePetrobrasTweetsUseCase {
     constructor(
         private tweetRepository: ITweetRepository,
         private twitterService: ITwitterService,
+        private messageService: IMessageService,
     ) {}
 
+    private queueName:string = 'informationToBeAnalysedSqsQueueName'
     private company:string[] = ['petrobras', 'PETR4']
     private tweets:Tweet[]
 
@@ -31,6 +34,9 @@ export class SavePetrobrasTweetsUseCase {
 
             await this.tweetRepository.saveMany(tweets)
             console.log('savePetrobrasTweetsUseCase::saved')
+
+            await this.messageService.send(this.queueName, JSON.stringify(tweets))
+            console.log('savePetrobrasTweetsUseCase::tweets message sent')
 
         } catch (error) {
             return Promise.resolve({
